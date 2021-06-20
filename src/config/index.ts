6e48b69dba36576a99e6f9eb2445211b9d7a3c03
rahi-nz <nz.rahele@gmail.com>
2021-06-21 01:16:48 +0430

    change confid file :bug:

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3030' : 'https://t9-converter-1';
+export const server = dev ? 'http://localhost:3030' : 'https://t9-converter-deploy';
