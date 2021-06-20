e49d7fd74daa8758b877721c4f9167e10e8cf26f
rahi-nz <nz.rahele@gmail.com>
2021-06-21 01:25:26 +0430

    change server config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3030' : 'https://t9-converter-deploy';
+export const server = dev ? 'http://localhost:3030' : 'https://t9-deploy.vercel.app';
